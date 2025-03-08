9ef1f1a43d2f54f996d0c5ea926cb4aef3e7d581
toony10 <amiranton257@gmail.com>
2025-03-08 04:31:57 +0200

    Hero Section2

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -10,7 +10,7 @@ const Header = () => {
                 <Image src={ assets.profile_img } alt='' className='rounded-full w-32' />
             </div>
             <h3 className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-ovo'>
-                Hi! I'm Anton Amir <Image src={ assets.hand_icon } alt='' className='w-6' />
+                Hi! I&apos;m Anton Amir <Image src={ assets.hand_icon } alt='' className='w-6' />
             </h3>
             <h1 className='text-3xl sm:text-6xl lg:text-[66px] font-ovo'>Front-end web developer</h1>
             <p className='max-w-2xl mx-auto font-ovo'>
