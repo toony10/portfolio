dc74ad2cd15048b4298ca431451d05c736f83cb5
toony10 <amiranton257@gmail.com>
2025-03-12 02:48:08 +0200

    work section

--- app/components/Work.tsx
+++ app/components/Work.tsx
@@ -1,12 +1,34 @@
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 import SectionHeader from './custom/SectionHeader'
-
-const Work = () => {
-    return (
-        <div className='w-full px-[12%] py-10 scroll-mt-20'>
-            <SectionHeader sm='My Work' lg='Projects' />
+import { workData } from '@/public/assets/assets'
+import { FaBriefcase, FaMapMarkerAlt, FaCalendarAlt } from 'react-icons/fa'
+const Work = () => (
+    <div className='w-full px-[12%] py-10 scroll-mt-20'>
+        <SectionHeader sm='My Work' lg='Projects' />
+        <div className="mt-12 flex flex-col relative max-w-3xl mx-auto gap-6">
+            { workData.map((project, index) => (
+                <div key={ index } className="relative flex flex-col items-center justify-between text-center border border-gray-300 rounded-lg p-6 shadow-md hover:shadow-2xl transition duration-300 bg-white">
+                    <Link href={ project.Live } target="_blank" rel="noopener noreferrer">
+                        <Image width={ 100 } height={ 100 } src={ project.bgImage } alt={ project.title } className="w-full h-64 object-cover rounded-lg" />
+                    </Link>
+                    <h3 className="text-2xl font-semibold text-gray-700 mt-4">{ project.title }</h3>
+                    <p className="text-gray-600 text-sm mt-2">{ project.description }</p>
+                    <div className="flex gap-4 mt-4">
+                        <Link
+                            href={ project.Live }
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="mt-4 px-6 py-2 bg-green-500 text-white rounded-full shadow-md hover:bg-green-700 transition-all"
+                        >
+                            View Live
+                        </Link>
+                    </div>
+                </div>
+            )) }
         </div>
-    )
-}
+    </div>
+)
 
 export default Work
--- public/assets/assets.ts
+++ public/assets/assets.ts
@@ -79,38 +79,24 @@ export const assets = {
 
 export const workData = [
     {
-        title: 'Frontend project',
-        description: 'Web Design',
+        title: 'Khazanty',
+        description: 'e-commerce website for a furniture store in Saudi Arabia using Next.js.',
         bgImage: '/work-1.png',
+        Live: 'https://www.khazantyksa.com/'
     },
     {
-        title: 'Geo based app',
-        description: 'Mobile App',
+        title: 'Hadarah',
+        description: 'website about ancient Egyptian life and Pharaonic civilization using Next.js and Tailwind CSS. Designed for easy navigation and clear information presentation as a graduation project for the Faculty of Archaeology',
         bgImage: '/work-2.png',
-    },
-    {
-        title: 'Photography site',
-        description: 'Web Design',
-        bgImage: '/work-3.png',
-    },
-    {
-        title: 'UI/UX designing',
-        description: 'UI/UX Design',
-        bgImage: '/work-4.png',
-    },
+        Live: 'https://hadhara-mfv5.vercel.app/'
+    }
 ]
 
-export const serviceData = [
-    { icon: assets.web_icon, title: 'Web design', description: 'Web development is the process of building, programming...', link: '' },
-    { icon: assets.mobile_icon, title: 'Mobile app', description: 'Mobile app development involves creating software for mobile devices...', link: '' },
-    { icon: assets.ui_icon, title: 'UI/UX design', description: 'UI/UX design focuses on creating a seamless user experience...', link: '' },
-    { icon: assets.graphics_icon, title: 'Graphics design', description: 'Creative design solutions to enhance visual communication...', link: '' },
-]
 
 export const infoList = [
     { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages & Technologies', description:['HTML, CSS, Bootstrap, Tailwind css, ShadCN, JavaScript, React Js, Next Js, Redux.js, Axios'] },
     { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'Bachelor of Computing and Information Technology - ', link: ['The Egyptian E-Learning University (EELU)','https://www.eelu.edu.eg/'] },
-    { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Built more than 5 projects using React and Next.js, including personal and work projects. Feel free to Explore them in',link: [' the next section.','/#work']}
+    { icon: assets.project_icon, iconDark: assets.project_icon_dark, title: 'Projects', description: 'Built more than 5 projects using React and Next.js, including personal and work projects. Feel free to Explore them in',link: [' the work section.','/#work']}
 ];
 
 export const toolsData = [
