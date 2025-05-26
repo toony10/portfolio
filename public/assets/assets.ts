8c144f3810617e7aa760377d0067c938891b1f5c
toony10 <amiranton257@gmail.com>
2025-05-27 02:09:38 +0300

    ex

--- public/assets/assets.ts
+++ public/assets/assets.ts
@@ -33,74 +33,92 @@ import right_arrow from './right-arrow.png';
 import send_icon from './send-icon.png';
 import right_arrow_bold from './right-arrow-bold.png';
 import right_arrow_bold_dark from './right-arrow-bold-dark.png';
-import whatsApp from "./WhatsApp.png";
-import linkedIn from "./LinkedIn.png";
+import whatsApp from './WhatsApp.png';
+import linkedIn from './LinkedIn.png';
 import gitHub from './github.png';
 import { link } from 'fs';
 
 export const assets = {
-    user_image,
-    code_icon,
-    code_icon_dark,
-    edu_icon,
-    edu_icon_dark,
-    skill_icon,
-    skill_icon_dark,
-    vscode,
-    firebase,
-    figma,
-    git,
-    postMan,
-    right_arrow_white,
-    logo,
-    logo_dark,
-    mail_icon,
-    mail_icon_dark,
-    profile_img,
-    download_icon,
-    download_icon_dark,
-    hand_icon,
-    header_bg_color,
-    moon_icon,
-    sun_icon,
-    arrow_icon,
-    arrow_icon_dark,
-    menu_black,
-    menu_white,
-    close_black,
-    close_white,
-    ui_icon,
-    right_arrow,
-    send_icon,
-    right_arrow_bold,
-    right_arrow_bold_dark,
-    whatsApp,
-    linkedIn,
-    gitHub
+  user_image,
+  code_icon,
+  code_icon_dark,
+  edu_icon,
+  edu_icon_dark,
+  skill_icon,
+  skill_icon_dark,
+  vscode,
+  firebase,
+  figma,
+  git,
+  postMan,
+  right_arrow_white,
+  logo,
+  logo_dark,
+  mail_icon,
+  mail_icon_dark,
+  profile_img,
+  download_icon,
+  download_icon_dark,
+  hand_icon,
+  header_bg_color,
+  moon_icon,
+  sun_icon,
+  arrow_icon,
+  arrow_icon_dark,
+  menu_black,
+  menu_white,
+  close_black,
+  close_white,
+  ui_icon,
+  right_arrow,
+  send_icon,
+  right_arrow_bold,
+  right_arrow_bold_dark,
+  whatsApp,
+  linkedIn,
+  gitHub,
 };
 
 export const workData = [
-    {
-        title: 'Khazanty',
-        description: 'e-commerce website for a furniture store in Saudi Arabia using Next.js, Tailwind CSS and Email js for users emails',
-        bgImage: '/work-1.png',
-        Live: 'https://www.khazantyksa.com/'
-    },
-    {
-        title: 'Hadarah',
-        description: 'website about ancient Egyptian life and Pharaonic civilization using Next.js and Tailwind CSS. Designed for easy navigation and clear information presentation as a graduation project for the Faculty of Archaeology',
-        bgImage: '/work-2.png',
-        Live: 'https://hadhara-mfv5.vercel.app/'
-    }
-]
-
+  {
+    title: 'Khazanty',
+    description:
+      'e-commerce website for a furniture store in Saudi Arabia using Next.js, Tailwind CSS and Email js for users emails',
+    bgImage: '/work-1.png',
+    Live: 'https://www.khazantyksa.com/',
+  },
+  {
+    title: 'Hadarah',
+    description:
+      'website about ancient Egyptian life and Pharaonic civilization using Next.js and Tailwind CSS. Designed for easy navigation and clear information presentation as a graduation project for the Faculty of Archaeology',
+    bgImage: '/work-2.png',
+    Live: 'https://hadhara-mfv5.vercel.app/',
+  },
+];
 
 export const infoList = [
-    { icon: assets.edu_icon, iconDark: assets.edu_icon_dark, title: 'Education', description: 'Bachelor of Computing and Information Technology - ', link: ['The Egyptian E-Learning University (EELU)','https://www.eelu.edu.eg/'] },
-    { icon: assets.code_icon, iconDark: assets.code_icon_dark, title: 'Languages & Technologies', description:['HTML, CSS, Bootstrap, Tailwind css, ShadCN, JavaScript, React Js, Next Js, Redux.js, SQL'] },
-    { icon: assets.skill_icon, iconDark: assets.skill_icon_dark, title: 'Skills', description: 'Hardworking, Detail oriented, Effective Communication, Time Management, Problem Solving and Creativity, Continuous Learning',}
+  {
+    icon: assets.edu_icon,
+    iconDark: assets.edu_icon_dark,
+    title: 'Education',
+    description: 'Bachelor of Computing and Information Technology - ',
+    link: ['The Egyptian E-Learning University (EELU)', 'https://www.eelu.edu.eg/'],
+  },
+  {
+    icon: assets.code_icon,
+    iconDark: assets.code_icon_dark,
+    title: 'Languages & Technologies',
+    description: [
+      'HTML, CSS, Bootstrap, Tailwind css, ShadCN, JavaScript, React Js, Next Js, zustand, SQL',
+    ],
+  },
+  {
+    icon: assets.skill_icon,
+    iconDark: assets.skill_icon_dark,
+    title: 'Skills',
+    description:
+      'Hardworking, Detail oriented, Effective Communication, Time Management, Problem Solving and Creativity, Continuous Learning',
+  },
 ];
 
-export const toolsData = [
-    assets.vscode, assets.firebase, assets.postMan, assets.git
-];
+export const toolsData = [assets.vscode, assets.firebase, assets.postMan, assets.git];
